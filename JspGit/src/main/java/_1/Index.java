c481fd511d01ebaeac85f78308495d6ed1ec187e
김선재 <@192.168.219.102>
2022-01-27 11:27:34 +0900

    Test.java

--- JspGit/src/main/java/_1/Index.java
+++ JspGit/src/main/java/_1/Index.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.RequestWrapper;
 
 @WebServlet("/index")
 public class Index extends HttpServlet {
@@ -19,7 +20,7 @@ public class Index extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("NewFile.jsp");
+		RequestWrapper dispatcher = request.getRequestDispatcher("NewFile.jsp");
 		dispatcher.forward(request, response);
 	}
 }
